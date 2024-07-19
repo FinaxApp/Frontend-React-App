8fe0f6f49579ef4484c6d87fc3192aa7af7530a5
“Ankit <22amtics124@gmail.com>
2024-07-19 13:22:46 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App font-bold">
+      <header className="App-header">
+        <img src={"./assets/logo.svg"} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
