7d0ef6def897b236b8096355f772d6e7373d9b6e
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2024-07-23 16:26:14 +0530

    removed unusevariables

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
 // Define the User type
@@ -19,15 +19,6 @@ interface User {
 
   export default function DashBoard({ user, setUser, setToken }: DashBoardProps) {
     const navigate = useNavigate();
-    const setCookie = (name: string, value: string, days: number) => {
-      let expires = "";
-      if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-      }
-      document.cookie = name + "=" + (value || "") + expires + "; path=/";
-    };
   
     const deleteCookie = (name: string) => {
       document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
