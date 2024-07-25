60df7a82428751d2b0411fafc7330279b1b3054f
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2024-07-25 12:43:32 +0530

    nav bar introduced

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,26 +1,35 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Navbar from './NavBarComponent';
 
-// Define the User type
 interface User {
   dp: string;
   name: string;
   email: string;
 }
 
-// Define the props interface for the DashBoard component
+
 interface DashBoardProps {
   user: User | null;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
   setToken: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-
-
 export default function DashBoard({ user, setUser, setToken }: DashBoardProps) {
   const REACT_APP_BASE_API_URL = "http://finax.up.railway.app";
   // const REACT_APP_BASE_API_URL = "http://localhost:8080";
+
+  const [isFormVisible, setFormVisible] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    gstNumber: '',
+    panNumber: '',
+    tdsNumber: '',
+    phone: '',
+    email: ''
+  });
 
   const navigate = useNavigate();
 
@@ -32,7 +41,7 @@ export default function DashBoard({ user, setUser, setToken }: DashBoardProps) {
     deleteCookie("token");
     setToken(null);
     setUser(null);
-    navigate("/")
+    navigate("/");
   };
 
   const getCookie = (name: string): string | null => {
@@ -51,25 +60,16 @@ export default function DashBoard({ user, setUser, setToken }: DashBoardProps) {
     const token = getCookie("token");
     if (token) {
       try {
-        console.log(token)
         const response = await axios.post(
           `${REACT_APP_BASE_API_URL}/api/company`,
-          {
-            name: "vijay sales",
-            address: "surat guj",
-            GSTNumber: "37793196389q1",
-            PANNumber: "3q8 ryn3q89wry3qnd98q3y",
-            TDSNumber: "dmo3w89fjnweouifh",
-            phone: "939031975091",
-            email: "wegvoilwm",
-          },
+          formData,
           {
             headers: {
               Authorization: token,
             },
           }
         );
-        
+
         console.log("company:", response.data);
       } catch (error: any) {
         console.error("Error fetching user data:", error);
@@ -79,20 +79,137 @@ export default function DashBoard({ user, setUser, setToken }: DashBoardProps) {
         }
       }
     }
-  }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [id]: value
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    createCompany();
+    setFormVisible(false);
+  };
+
+  const toggleFormVisibility = () => {
+    setFormVisible(!isFormVisible);
+  };
 
   return (
-    <div>
-      <p>HI {user?.name}</p>
-      <img
-        src={user ? user.dp : "https://auth-db1131.hstgr.io/themes/pmahomme/img/logo_right.png"}
-        alt="User Profile"
-        onError={(e) => {
-          (e.target as HTMLImageElement).src = "https://auth-db1131.hstgr.io/themes/pmahomme/img/logo_right.png";
-        }}
-      />
-      <button onClick={logout} className='bg-red-600 p-3 m-4 rounded-md text-white' type="button">Logout</button>
-      <button type='button' onClick={createCompany} className='bg-blue-600 p-3 m-4 rounded-md text-white'>create company</button>
+    <div className="min-h-screen flex flex-col">
+
+      <div>{user?.name}</div>
+      <Navbar user={user} logout={logout} />
+      <div className="flex-grow p-8 bg-gray-100">
+        <button 
+          type='button' 
+          onClick={toggleFormVisibility} 
+          className='bg-blue-600 p-3 m-4 rounded-md text-white'
+        >
+          Create Company
+        </button>
+        {isFormVisible && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="relative bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+              <button
+                type="button"
+                className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 text-2xl"
+                onClick={toggleFormVisibility}
+              >
+                &times;
+              </button>
+              <form onSubmit={handleSubmit}>
+                <div className="grid grid-cols-1 gap-4">
+                  <div className="mb-4">
+                    <label htmlFor="name" className="block text-gray-700 mb-2">Name</label>
+                    <input
+                      type="text"
+                      id="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="address" className="block text-gray-700 mb-2">Address</label>
+                    <input
+                      type="text"
+                      id="address"
+                      value={formData.address}
+                      onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    />
+                  </div>
+                  <div className="mb-4 grid grid-cols-2 gap-4">
+                    <div>
+                      <label htmlFor="gstNumber" className="block text-gray-700 mb-2">GST Number</label>
+                      <input
+                        type="text"
+                        id="gstNumber"
+                        value={formData.gstNumber}
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="panNumber" className="block text-gray-700 mb-2">PAN Number</label>
+                      <input
+                        type="text"
+                        id="panNumber"
+                        value={formData.panNumber}
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4 grid grid-cols-2 gap-4">
+                    <div>
+                      <label htmlFor="tdsNumber" className="block text-gray-700 mb-2">TDS Number</label>
+                      <input
+                        type="text"
+                        id="tdsNumber"
+                        value={formData.tdsNumber}
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="phone" className="block text-gray-700 mb-2">Phone</label>
+                      <input
+                        type="tel"
+                        id="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="email" className="block text-gray-700 mb-2">Email</label>
+                    <input
+                      type="email"
+                      id="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-purple-500 text-white py-2 rounded-lg hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                >
+                  Create Company
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBarComponent.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface User {
+  dp: string;
+  name: string;
+  email: string;
+}
+
+interface NavbarProps {
+  user: User | null;
+  logout: () => void;
+}
+
+const Navbar = ({ user, logout }: NavbarProps) => {
+  return (
+    <nav className="bg-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-gray-800 text-lg font-bold"><img className="mx-auto w-24" src="assets/images/finax2.png" alt="Your Company" /></Link>
+        <div className="flex items-center">
+          {user ? (
+            <div className="text-gray-800 mr-4 flex items-center">
+              <span>{user.name}</span>
+              <img src={user.dp} alt="User Profile" className="inline-block w-8 h-8 rounded-full ml-2 border border-gray-300" />
+            </div>
+          ) : (
+            <span className="text-gray-800 mr-4">Guest</span>
+          )}
+          <button onClick={logout} className="hover:bg-black p-2 rounded-md hover:text-white hover:bg-red-700 border-black border"><span className="mr-2 ml-1">Logout</span><i className="fa fa-sign-out ml-2"></i>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
