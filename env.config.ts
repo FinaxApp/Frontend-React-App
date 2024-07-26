4f5eee02255dd00a288f0662af09a6aa81da7b99
“Ankit <22amtics124@gmail.com>
2024-07-26 09:52:55 +0530

    env.config.ts creted

--- env.config.ts
+++
@@ -1,4 +0,0 @@
-const REACT_APP_SIGNIN_WITH_GOOGLE_URL = "http://localhost:8080/auth/google";
-const REACT_APP_BASE_API_URL = "http://localhost:8080"
-
-export default {REACT_APP_BASE_API_URL, REACT_APP_SIGNIN_WITH_GOOGLE_URL}
---
+++ src/utils/env.config.ts
@@ -0,0 +1,8 @@
+const env = {
+     BASE_API_URL :"https://api.jsrsoftcare.com",
+     SIGNIN_WITH_GOOGLE_URL :"https://api.jsrsoftcare.com/auth/google"
+
+}
+
+
+export {env}
