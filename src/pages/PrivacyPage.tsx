dd7ba4c4b4cfeee8b2767a58b3ce8d04957b52c1
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2024-07-26 17:31:55 +0530

    login testing

--- src/pages/PrivacyPage.tsx
+++ src/pages/PrivacyPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PrivacyPolicy from '../components/PrivacyComponent'
-import Navbar from '../components/NavBarComponent'
 
 export default function PrivacyPage() {
   return (
